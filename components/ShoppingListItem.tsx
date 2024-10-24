6f1ae67bf30d3a3d853fc2834b1087bd98d02395
Muntajir Hussain Gazi <muntajirwork11@gmail.com>
2024-10-24 18:46:51 +0530

    delete button UI change

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -1,11 +1,13 @@
 import { TouchableOpacity, View, Alert, StyleSheet, Text } from "react-native";
 import { theme } from "../theme";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 type Props = {
   name: string;
+  isCompleted?: boolean;
 };
 
-export function ShoppingListItem({ name }: Props) {
+export function ShoppingListItem({ name, isCompleted }: Props) {
   const handleDelete = () => {
     Alert.alert(
       `Are you sure you want to delete ${name}?`,
@@ -17,19 +19,23 @@ export function ShoppingListItem({ name }: Props) {
           style: "destructive",
         },
         { text: "Cancel", style: "cancel" },
-      ],
+      ]
     );
   };
 
   return (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemText}>{name}</Text>
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text style={[styles.itemText,, isCompleted ? styles.completedText : undefined]}>{name}</Text>
       <TouchableOpacity
         onPress={handleDelete}
-        style={styles.button}
         activeOpacity={0.8}
       >
-        <Text style={styles.buttonText}>Delete</Text>
+        <AntDesign name="closecircle" size={24} color={isCompleted ? theme.colorGray: theme.colorRed } />
       </TouchableOpacity>
     </View>
   );
@@ -38,26 +44,24 @@ export function ShoppingListItem({ name }: Props) {
 const styles = StyleSheet.create({
   itemContainer: {
     paddingVertical: 16,
-    paddingHorizontal: 8,
+    paddingHorizontal: 18,
     borderBottomColor: theme.colorCerulean,
     borderBottomWidth: 1,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
   },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+
   itemText: {
     fontSize: 18,
     fontWeight: "200",
   },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: "#fff",
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: 1,
+
+  completedText: {
+    textDecorationLine: "line-through",
   },
 });
